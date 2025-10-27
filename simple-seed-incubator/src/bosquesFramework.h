13991858b8742da1a6130d44b9cd351a9f38af99
masch <the.masch@gmail.com>
2025-10-27 16:28:27 -0300

    Update WiFi credentials and improve code formatting in configuration and framework files

--- simple-seed-incubator/src/bosquesFramework.h
+++ simple-seed-incubator/src/bosquesFramework.h
@@ -7,25 +7,30 @@
 
 HTTPClient http;
 
-//Regresa bool invertido basado en conexion a red WiFi
-bool wifiSetup(const char* ssid, const char* passWifi){
+// Regresa bool invertido basado en conexion a red WiFi
+bool wifiSetup(const char *ssid, const char *passWifi)
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, passWifi);
-  Serial.println("Conectando a WiFi");
+  Serial.println("Conectando a WiFi" + String(ssid) + " ...");
 
-  for (int i=0; i<10; i++){
-    if (WiFi.status() != WL_CONNECTED) {
+  for (int i = 0; i < 10; i++)
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
       Serial.print('.');
-      vTaskDelay (1000);
+      vTaskDelay(1000);
     }
-    else{
+    else
+    {
       Serial.println("Conectado al Wifi \n");
       return false;
       break;
     }
   }
 
-  if (WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("Error al conectarse al WiFi \n");
     WiFi.disconnect(true, false);
     return true;
@@ -34,72 +39,84 @@ bool wifiSetup(const char* ssid, const char* passWifi){
   Serial.println(WiFi.localIP());
 }
 
-//Checkeo de conexion a internet
-bool modeSetup (String url){
+// Checkeo de conexion a internet
+bool modeSetup(String url)
+{
   const int kNetworkTimeout = 2000;
   const int kNetworkDelay = 1000;
 
   http.begin(url.c_str());
   unsigned long timeoutStart = millis();
 
+  Serial.println("Verificando conexion a Internet:... " + url);
+
   while (http.connected() &&
-        (millis() - timeoutStart) < kNetworkTimeout){
-          int response = http.GET();
-          Serial.print(response);
-          if (response == 200){
-              Serial.println(" - Modo de trabajo: ONLINE \n");
-              timeoutStart = millis();
-              http.end();
-              delay(kNetworkDelay);
-              return false;
-          }
-          else{
-          Serial.println("Modo de trabajo: OFFLINE \n");
-          delay(5000);
-          http.end();
-          return true;
-          }
+         (millis() - timeoutStart) < kNetworkTimeout)
+  {
+    int response = http.GET();
+    Serial.print(response);
+    if (response == 200)
+    {
+      Serial.println(" - Modo de trabajo: ONLINE \n");
+      timeoutStart = millis();
+      http.end();
+      delay(kNetworkDelay);
+      return false;
     }
+    else
+    {
+      Serial.println("Modo de trabajo: OFFLINE \n");
+      delay(5000);
+      http.end();
+      return true;
+    }
+  }
 }
 
-//Devuelve temp seteada en servidor
-float getServerTemp(String url, float setTemp){  
+// Devuelve temp seteada en servidor
+float getServerTemp(String url, float setTemp)
+{
   float serverTemp = setTemp;
 
-    http.begin(url.c_str());  
-    int responseCode = http.GET();
-    String responseTemp = http.getString();
+  http.begin(url.c_str());
+  int responseCode = http.GET();
+  String responseTemp = http.getString();
 
-    if (responseCode == 200) {
-      serverTemp = responseTemp.toFloat();
-      }
-    else{
-      Serial.println("Error: ");
-      Serial.printf("%i \n", responseCode);
-      }
-    Serial.printf("CONTROL - Temperatura leida del Servidor: %f // \n ", serverTemp);
-    http.end();
+  if (responseCode == 200)
+  {
+    serverTemp = responseTemp.toFloat();
+  }
+  else
+  {
+    Serial.println("Error: ");
+    Serial.printf("%i \n", responseCode);
+  }
+  Serial.printf("CONTROL - Temperatura leida del Servidor: %f // \n ", serverTemp);
+  http.end();
   return serverTemp;
 }
 
-//Envia temp actual a server
-void updateServerTemp(String url, float newtemp){
+// Envia temp actual a server
+void updateServerTemp(String url, float newtemp)
+{
   char buffer[6];
-  String sentTemp = floatToString(newtemp,buffer,6,2);
+  String sentTemp = floatToString(newtemp, buffer, 6, 2);
 
-    http.begin(url.c_str());
-    http.addHeader("temp", sentTemp);
-    int responseCode = http.POST("");
-    
-    if (responseCode == 200){
-      Serial.println("Temperatura enviada correctamente a servidor");
-    }
-    else{
-      Serial.println("Error: ");
-      Serial.print(" " + responseCode);
-      Serial.println("");
-    }
-    http.end();
+  http.begin(url.c_str());
+  http.addHeader("temp", sentTemp);
+  int responseCode = http.POST("");
+
+  if (responseCode == 200)
+  {
+    Serial.println("Temperatura enviada correctamente a servidor");
+  }
+  else
+  {
+    Serial.println("Error: ");
+    Serial.print(" " + responseCode);
+    Serial.println("");
+  }
+  http.end();
 }
 
 #endif
